2f5f4873af42d4e7b2c33301186e16a3018fa63b
Kooper L. Smith <klsmith6@bsu.edu>
2014-04-22 22:08:00 -0400

    Removed comment.

--- src/com/pkw/find/violet/Block.java
+++ src/com/pkw/find/violet/Block.java
@@ -1,8 +1,9 @@
 package com.pkw.find.violet;
 
 import java.awt.Color;
-//Hello World
+
 public enum Block {
+
 	EMPTY(0, "    ", new Color(0, 0, 0)), //
 	TWO(2, "  2 ", new Color(255, 0, 0)), //
 	FOUR(4, "  4 ", new Color(255, 136, 0)), //
